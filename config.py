8e7212ddcc3622c4a3348b0b5fad49a63cc62ee8
soyyoon <syzzang7564@gmail.com>
2024-09-06 14:08:39 +0900

    Resolved merge conflict in config.py

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
 class Config:
-<<<<<<< HEAD
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = '1234' 
     MYSQL_DB = 'RENTAL_SSU' 
     MYSQL_PORT = 3306
     MYSQL_CURSORCLASS = "DictCursor"
-=======
-    MYSQL_HOST = 'soyoon.mysql.pythonanywhere-services.com'
-    MYSQL_USER = 'soyoon'
-    MYSQL_PASSWORD = 'rental_ssu' 
-    MYSQL_DB = 'rental_ssu' 
-    
-    MYSQL_PORT = 3306
->>>>>>> 5e4112b407d67b13f9a6cd149379471275d1a08a
